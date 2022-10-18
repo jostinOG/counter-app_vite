69b21706261a9c7d413cb22dd436868356f5c361
jostinOG <Jostinfabian14@gmail.com>
2022-10-18 12:07:20 +0200

    Return elements in Component - Fragment

---
+++ src/FirstApp.jsx
@@ -0,0 +1,10 @@
+import {Fragment} from "react";
+
+export const FirstApp = () => {
+    return (
+        <>
+            <h2>Jostin</h2>
+            <p>CBCB</p>
+        </>
+    );
+}
--- src/HelloWorldApp.jsx
+++ src/HelloWorldApp.jsx
@@ -1,5 +1,5 @@
 export const HelloWorldApp = () => {
     return (
-        <h1>Hello World!!!!</h1>
-    )
+        <h1>Hello World app!!!</h1>
+    );
 }
